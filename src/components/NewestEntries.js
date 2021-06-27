260959c0cc6b77f0f31c8cafba2bb9fc2eb306e3
Carlos Yarza <carlosyarza@gmail.com>
2021-06-28 00:25:41 +0200

    post bunker from editor in new entries component

--- src/components/NewestEntries.js
+++ src/components/NewestEntries.js
@@ -1,5 +1,50 @@
 import { GoArrowUp, GoArrowDown } from "react-icons/go";
+import { useState, useEffect } from "react"
+import axios from "axios";
+const Row = ({author, title, body, createdAt}) => (
+    <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
+    <td class="px-4 py-3">
+      <div>
+        <p class="font-semibold">{title}</p>
+        <p class="text-xs text-gray-600 dark:text-gray-400">
+          <div dangerouslySetInnerHTML={{ __html: body }} />
+        </p>
+      </div>
+    </td>
+    <td class="px-4 py-3 text-sm">{author.name}</td>
+    <td class="px-4 py-3 text-xs">
+      <td class="px-2 py-1 text-xs">
+        <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+          50⬆
+        </span>
+      </td>
+      <td class="px-2 py-1 text-xs">
+        <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
+          40⬇
+        </span>
+      </td>
+    </td>
+  </tr>
+)
+
 const NewestEntries = () => {
+  const [bunkers, setBunkers] = useState([])
+
+  const loadBunkers = async () => {
+    try {
+      const { data } = await axios.get("/bunkers")
+      console.log(bunkers);
+      setBunkers([...data])    
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    loadBunkers()
+  }, [])
+
   return (
     <div className="flex-col h-110 ">
       <div>
@@ -20,126 +65,7 @@ const NewestEntries = () => {
                 Newest Entries
               </tr> */}
               <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
-                <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
-                  <td class="px-4 py-3">
-                    <div>
-                      <p class="font-semibold">This is a title.</p>
-                      <p class="text-xs text-gray-600 dark:text-gray-400">
-                        Neque porro quisquam est qui dolorem ipsum quia dolor
-                        sit amet, consectetur, adipisci velit
-                      </p>
-                    </div>
-                  </td>
-                  <td class="px-4 py-3 text-sm">author_name</td>
-                  <td class="px-4 py-3 text-xs">
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                        50⬆
-                      </span>
-                    </td>
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
-                        40⬇
-                      </span>
-                    </td>
-                  </td>
-                </tr>
-                <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
-                  <td class="px-4 py-3">
-                    <div>
-                      <p class="font-semibold">This is a title.</p>
-                      <p class="text-xs text-gray-600 dark:text-gray-400">
-                        Neque porro quisquam est qui dolorem ipsum quia dolor
-                        sit amet, consectetur, adipisci velit
-                      </p>
-                    </div>
-                  </td>
-                  <td class="px-4 py-3 text-sm">author_name</td>
-                  <td class="px-4 py-3 text-xs">
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                        50⬆
-                      </span>
-                    </td>
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
-                        40⬇
-                      </span>
-                    </td>
-                  </td>
-                </tr>
-                <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
-                  <td class="px-4 py-3">
-                    <div>
-                      <p class="font-semibold">This is a title.</p>
-                      <p class="text-xs text-gray-600 dark:text-gray-400">
-                        Neque porro quisquam est qui dolorem ipsum quia dolor
-                        sit amet, consectetur, adipisci velit
-                      </p>
-                    </div>
-                  </td>
-                  <td class="px-4 py-3 text-sm">author_name</td>
-                  <td class="px-4 py-3 text-xs">
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                        50⬆
-                      </span>
-                    </td>
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
-                        40⬇
-                      </span>
-                    </td>
-                  </td>
-                </tr>
-                <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
-                  <td class="px-4 py-3">
-                    <div>
-                      <p class="font-semibold">This is a title.</p>
-                      <p class="text-xs text-gray-600 dark:text-gray-400">
-                        Neque porro quisquam est qui dolorem ipsum quia dolor
-                        sit amet, consectetur, adipisci velit
-                      </p>
-                    </div>
-                  </td>
-                  <td class="px-4 py-3 text-sm">author_name</td>
-                  <td class="px-4 py-3 text-xs">
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                        50⬆
-                      </span>
-                    </td>
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
-                        40⬇
-                      </span>
-                    </td>
-                  </td>
-                </tr>
-                <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
-                  <td class="px-4 py-3">
-                    <div>
-                      <p class="font-semibold">This is a title.</p>
-                      <p class="text-xs text-gray-600 dark:text-gray-400">
-                        Neque porro quisquam est qui dolorem ipsum quia dolor
-                        sit amet, consectetur, adipisci velit
-                      </p>
-                    </div>
-                  </td>
-                  <td class="px-4 py-3 text-sm">author_name</td>
-                  <td class="px-4 py-3 text-xs">
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                        50⬆
-                      </span>
-                    </td>
-                    <td class="px-2 py-1 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
-                        40⬇
-                      </span>
-                    </td>
-                  </td>
-                </tr>
+                {bunkers.map((bunker) => <Row  {...bunker}/> )}
               </tbody>
             </table>
           </div>

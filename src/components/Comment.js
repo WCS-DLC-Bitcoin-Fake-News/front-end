699bf7e4ca53b974dcf9e753f0b2af95d8b10608
Agnes <mate.agnesvagyok@gmail.com>
2021-07-07 16:04:04 +0200

    added 2 new components, 1 for the comment content and 1 for the comment nesting

---
+++ src/components/Comment.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Comment, Avatar } from "antd";
+
+const CommentContent = ({ children }) => (
+  <Comment
+    actions={[<span key="comment-nested-reply-to">Reply to</span>]}
+    author={<a>User1</a>}
+    avatar={
+      <Avatar
+        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+        alt="User1"
+      />
+    }
+    content={
+      <p>
+        We supply a series of design principles, practical patterns and high
+        quality design resources (Sketch and Axure).
+      </p>
+    }
+  >
+    {children}
+  </Comment>
+);
+
+export default CommentContent;
---
+++ src/components/CommentContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CommentContainer = () => {
+  return (
+    <UserComment>
+      <UserComment>
+        <UserComment />
+        <UserComment />
+      </UserComment>
+    </UserComment>
+  );
+};
+export default CommentContainer;

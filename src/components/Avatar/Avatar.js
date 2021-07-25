159e0386ed85b09cfc6c74e7ad86828419e80ae6
Pierre <pierregiddio@gmail.com>
2021-07-25 11:36:04 +0200

    Wip Comments Threads after vote

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Avatar = ({ src }) => {
   return (
-    <img className="w-full h-full object-cover" src="public/images/logos/user.svg" alt="user_avatar" />
+    <img className="w-full h-full object-cover" src="https://i.pravatar.cc/300" alt="user_avatar" />
   );
 };
 
---
+++ src/components/Post/PostButtons.js
@@ -0,0 +1,92 @@
+import BookmarkIcon from "@material-ui/icons/Bookmark";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ThumbDownIcon from "@material-ui/icons/ThumbDown";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import UserContext from "../../contexts/UserContext";
+import { deleteBunker } from "../../Api/DeleteBunker";
+import { fetchBunkerLikes, fetchBunkerSaves } from "../../Api/FetchData";
+import Avatar from "../Avatar/Avatar";
+import BunkerVisualizer from "../modules/Bunker/BunkerVisualizer";
+import axios from "axios";
+
+const PostButtons = ({bunker, likeBunker, dislikeBunker, saveBunkers, unsaveBunkers, isSaved }) => {
+  return(
+    <>
+    <hr></hr>
+
+    <div className="flex flex-row my-2 items-stretch">
+
+        <button
+          onClick={(e) => e.stopPropagation() && likeBunker()}
+          className="flex-1 mx-4 font-noto font-medium rounded-lg hover:bg-gray-400 cursor-pointer py-6"
+          type="submit"
+          >
+          <span className="">
+            <ChatBubbleOutlineIcon style={{ color: "#828282" }} />
+          </span>
+          <span className="hidden lg:block">Comments</span>
+        </button>
+        <button
+            className="flex-1 mx-4 font-noto font-medium rounded-lg hover:bg-gray-400 cursor-pointer py-6"
+            type="submit"
+            onClick={(e) => {
+              e.stopPropagation();
+              likeBunker();
+            }}>
+            <span className="">
+              <ThumbUpIcon style={{ color: "#828282" }} />
+            </span>
+            <span className="hidden lg:block">Upvote</span>
+          </button>
+          <button
+            className="flex-1 mx-4 font-noto font-medium rounded-lg hover:bg-gray-400 cursor-pointer py-6"
+            type="submit"
+            onClick={(e) => {
+              e.stopPropagation();
+              dislikeBunker();
+            }}>
+            <span className="">
+              <ThumbDownIcon style={{ color: "#828282" }} />
+            </span>
+            <span className="hidden lg:block">Downvote</span>
+          </button>
+        {isSaved ? (
+          <button
+            className="flex-1 mx-4 font-noto font-medium rounded-lg text-blue-600 hover:bg-gray-400 cursor-pointer py-6"
+            type="submit"
+            onClick={(e) => {
+              e.stopPropagation();
+              unsaveBunkers();
+            }}>
+            <span className="">
+              <BookmarkIcon style={{ color: "#2D9CDB" }} />
+            </span>
+            <span className="hidden lg:block">Saved</span>
+          </button>
+        ) : (
+          <button
+            className="flex-1 mx-4 font-noto font-medium rounded-lg hover:bg-gray-400 cursor-pointer py-6"
+            type="submit"
+            onClick={(e) => {
+              e.stopPropagation();
+              saveBunkers();
+            }}>
+            <span className="">
+              <BookmarkBorderIcon style={{ color: "#828282" }} />
+            </span>
+            <span className="hidden lg:block">Save</span>
+          </button>
+        )}
+      </div>
+      </>
+  )
+}
+
+
+export default PostButtons;

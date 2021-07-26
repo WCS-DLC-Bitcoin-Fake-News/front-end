ca3ddeab1cd9c3a93a7491446abe52693e947a39
Carlos Yarza <carlosyarza@gmail.com>
2021-07-26 23:31:24 +0200

    logos and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import AuthForm from "../components/AuthForm/AuthForm";
-
+import logo from "../assets/debunker_logo_title_300.png";
 const Home = () => {
   return (
     <div>
-      <div className="flex flex-col-reverse lg:flex-row lg:justify-center bg-bunker-blur-bg h-screen">
+      <div className="flex flex-col-reverse lg:flex-row lg:justify-center h-screen">
         <div className="lg:w-2/4 w-full h-full">
           <div className="w-full h-full bg-no-repeat bg-cover bg-top flex flex-col justify-center">
             <h1 className="text-black font-Cabin-400 font-bold mx-auto">
@@ -12,10 +12,10 @@ const Home = () => {
           </div>
         </div>
         <div className="lg:w-2/4 nm-inset-gray-300 py-6">
-          <div className="mx-6">
-            {/* <div className="my-4">
-              <img src="/images/logos/bunker.png" alt="logo" />
-            </div> */}
+          <div className="mx-6 mt-44">
+            <div className="my-4">
+              <img src={logo} alt="logo" />
+            </div>
             <div className="m-auto">
               <AuthForm type="signIn" />
             </div>

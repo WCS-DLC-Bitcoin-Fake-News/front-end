04c9f2de31f3c2a3f1bf40a18e77761fe56d1cea
Carlos Yarza <carlosyarza@gmail.com>
2021-07-27 17:19:13 +0200

    Wallet styling

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -2,10 +2,12 @@ import { useState } from "react";
 
 const Wallet = ({text, count, currency=`$`}) => {
     return(
-        <div className="nm-concave-yellowBunker rounded-full p-2">
+        <div>
             <h5>{text} {count} {currency}</h5>
         </div>
     )
 }
 
 export default Wallet;
+
+// className="nm-concave-yellowBunker rounded-full p-2"

6a5de9ac7a7b6d54c90ed56fa134706e1be7242f
AliaLoui <makarona.aliki@gmail.com>
2021-07-28 14:30:08 +0200

    editing profile

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -6,21 +6,26 @@ import { useFollowings } from "../../hooks/useFollowings";
 import Avatar from "../Avatar/Avatar";
 import FollowButton from "../FollowButton/FollowButton";
 import Modal from "../Modal/Modal";
+import Wallet from "../Wallet/Wallet";
 
 const UserInfo = ({ fetchedUser }) => {
   const { user } = useContext(UserContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isModalLoading, setIsModalLoading] = useState(true);
 
-  const { followers, getFollowers } = useFollowers(fetchedUser.uid);
-  const { followings, getFollowings } = useFollowings(fetchedUser.uid);
+  // const { followers, getFollowers } = useFollowers(fetchedUser.uid);
+  // const { followings, getFollowings } = useFollowings(fetchedUser.uid);
+
+  const followers = 12;
+  const following = 10;
 
   return (
     <div className="h-full flex flex-col lg:flex-row lg:items-center rounded-xl bg-white w-full">
       <div className=" mx-auto inline px-4">
         <div
           className="relative rounded-md lg:rounded-xl overflow-hidden bg-white  lg:p-2 h-24 w-24 lg:w-40 lg:h-40"
-          style={{ top: "-20px" }}>
+          style={{ top: "-20px" }}
+        >
           <Avatar src={fetchedUser.avatar} />
         </div>
       </div>
@@ -35,23 +40,24 @@ const UserInfo = ({ fetchedUser }) => {
               className="inline mr-2 text-xs font-poppins font-semibold hover:underline cursor-pointer"
               onClick={async () => {
                 setIsModalOpen(true);
-                await getFollowers();
+                // await getFollowers();
                 setIsModalLoading(false);
-              }}>
-              {fetchedUser.followersCount - 1}
+              }}
+            >
+              {followers}
               <span className="m-1 text-gray-500">Followers</span>
             </p>
-            <p
-              className="inline m-2 text-xs font-poppins font-semibold hover:underline cursor-pointer"
-              onClick={() => getFollowings()}>
-              {fetchedUser.followingsCount - 1}
+            <p className="inline m-2 text-xs font-poppins font-semibold hover:underline cursor-pointer">
+              {following}
+
               <span className="m-1 text-gray-500">Following</span>
             </p>
           </span>
           {user && fetchedUser.username === user.username ? (
             <button
               className="lg:mr-0 lg:ml-auto bg-primary text-white px-2 py-4  lg:px-8 lg:py-4 rounded-md"
-              type="submit">
+              type="submit"
+            >
               <span className="mx-2">
                 <PersonAddIcon />
               </span>
@@ -69,9 +75,7 @@ const UserInfo = ({ fetchedUser }) => {
               {fetchedUser.bio}
             </p>
           ) : (
-            <p className="font-noto text-lg my-2 text-secondary">
-              404 Bio Not Found
-            </p>
+            <Wallet text={"My balance is"} count={user.wallet} />
           )}
         </div>
       </div>
@@ -82,7 +86,8 @@ const UserInfo = ({ fetchedUser }) => {
             left: "50%",
             top: "0",
             transform: "translate(-50%, 20%)",
-          }}>
+          }}
+        >
           <Modal
             users={followers}
             close={() => setIsModalOpen(false)}

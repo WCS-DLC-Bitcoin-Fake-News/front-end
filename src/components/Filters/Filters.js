c4b4a4364e836510ba772f513fc5d5e16c934fb6
Carlos Yarza <carlosyarza@gmail.com>
2021-07-29 10:45:45 +0200

    Spacing filters

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -3,10 +3,12 @@ const ProgressBar = (props) => {
   const { bgcolor, completed } = props;
   const containerStyles = {
     lineHeight: "2rem",
-    width: "250px",
+    width: "380px",
     backgroundColor: "#e0e0de",
     borderRadius: 50,
-    margin: 50,
+    marginTop: 10,
+    marginLeft: 30,
+    marginBottom: 40,
   }
   const fillerStyles = {
     height: '100%',
@@ -39,30 +41,33 @@ const Filters = () => {
     <div
       className="w-full bg-white rounded-lg p-5"
       style={{ height: "max-content" }}>
-      <ProgressBar className="mr-" bgcolor="#9EAEC0" completed={70} />
-      <li className="font-montserrat font-semibold text-lg  py-4 list-none">
-        Pro
+      <li className="font-montserrat font-semibold text-lg flex justify-center py-2 list-none">
+        Consensus
       </li>
-          <li className="font-raleway text-lg py-4 list-none">
-            75 Votes
+      <ProgressBar bgcolor="#9EAEC0" completed={70} />
+      <li className="font-montserrat font-semibold text-lg  py-2 list-none">
+        Pro ✅
+      </li>
+          <li className="font-raleway text-lg py-2 list-none">
+            75 Users - Bounty 40%
           </li>
-      <li className="font-montserrat font-semibold text-lg text-black py-4 list-none ">
-        Con
+      <li className="font-montserrat font-semibold text-lg text-black py-2 list-none ">
+        Con ❎
       </li>
-           <li className="font-raleway text-lg py-4 list-none">
-              32 Votes
+           <li className="font-raleway text-lg py-2 list-none">
+              32 Users - Bounty 0%
           </li>
-      <li className="font-montserrat font-semibold text-lg text-black py-4 list-none ">
-        Author earned
+      <li className="font-montserrat font-semibold text-lg text-black py-2 list-none ">
+        Author earned 🤲
       </li>
-        <li className="font-raleway text-lg py-4 list-none">
-              1531 Votes
+        <li className="font-raleway text-lg py-2 list-none">
+              1531 Tokens
           </li>
-      <li className="font-montserrat font-semibold text-lg text-black py-4 list-none ">
-        Total Stake
+      <li className="font-montserrat font-semibold text-lg text-black py-2 list-none ">
+        Total Stake 💰
       </li>
-          <li className="font-raleway text-lg py-4 list-none">
-              500 Votes
+          <li className="font-raleway text-lg py-2 list-none">
+              500 Tokens
           </li>
         {/* <div>
           <label className="py-2 font-montserrat font-semibold text-lg" for="votes bar">Votes</label>
